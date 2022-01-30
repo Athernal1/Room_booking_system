5ce92c3535793f3ed9d726a12f4626ba578c877e
Michal Kludka <michalkludka5@gmail.com>
2022-01-30 12:21:40 +0100

    z

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 from django.views import View
 from .models import Room
+
 
 class HomePage(View):
     template_name = 'main_app/home_page.html'
@@ -18,6 +19,7 @@ class AddRoom(View):
     def post(self, request):
         name = request.POST.get('room-name')
         capacity = request.POST.get('capacity')
+        capacity = int(capacity) if capacity else 0
         projector = request.POST.get('projector')
 
         if projector:
@@ -31,9 +33,16 @@ class AddRoom(View):
         for room in all_rooms:
             all_rooms_names.append(room.name)
 
-        message_error = ''
         if name in all_rooms_names:
-            message_error += 'This name is already in use. Choose other name.\n'
-        if not name:
-            message_error += 'Name of room can\'t be empty'
+            error = 'This name is already in use. Choose other name.'
+            return render(request, self.template_name, context = {'error': error})
+        if not name or name == '':
+            error = 'Room name can\'t be empty'
+            return render(request, self.template_name, context={'error': error})
+        if capacity <= 0:
+            error = 'Capacity can\'t be so low'
+            return render(request, self.template_name, context={'error': error})
 
+        Room.objects.create(name=name, capacity=capacity, projector=projector)
+
+        return redirect('/')
